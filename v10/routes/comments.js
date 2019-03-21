a108cee38315ca0677711a62987b6b236a134082
Xkushal <kushalyhakuri123@gmail.com>
2019-03-21 12:51:42 -0500

    version 10 added

---
+++ v10/routes/comments.js
@@ -0,0 +1,77 @@
+var express=require("express");
+var router = express.Router({mergeParams:true});
+var Apartments = require("../models/apartments");
+var Comment = require("../models/comment");
+var middleware = require("../middleware");
+
+//comments new 
+router.get("/new",middleware.isLoggedIn, function(req, res) {
+    //find apartment by id
+    Apartments.findById(req.params.id, function(err, apartment){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("comments/new",{apartment: apartment});
+        }
+    });
+});
+
+//comments create
+router.post("/", middleware.isLoggedIn, function(req, res){
+    //look apartment by id
+    Apartments.findById(req.params.id,function(err, apartment) {
+        if(err){
+            console.log(err);
+            res.redirect("/apartments");
+        }else{
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err);
+                }else{
+                    //add username and if to comment and save
+                    comment.author.id = req.user._id;
+                    comment.author.username = req.user.username;
+                    comment.save();
+                    apartment.comments.push(comment);
+                    apartment.save();
+                    res.redirect('/apartments/' + apartment._id);
+                }
+            });
+        }
+    });
+});
+
+//edit route
+router.get("/:comment_id/edit", middleware.checkCommentOwner, function(req, res){
+    Comment.findById(req.params.comment_id, function(err, foundComment) {
+        if(err){
+            res.redirect("back");
+        }else{
+             res.render("comments/edit",{apartment_id: req.params.id, comment: foundComment});
+        }
+    });
+});
+
+//comment update
+router.put("/:comment_id",middleware.checkCommentOwner, function(req, res){
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+        if(err){
+            res.redirect("back");
+        }else{
+            res.redirect("/apartments/" + req.params.id);
+        }
+    });
+});
+
+//comments destroy routes
+router.delete("/:comment_id",middleware.checkCommentOwner, function(req, res){
+   Comment.findByIdAndRemove(req.params.comment_id, function(err){
+       if(err){
+           res.redirect("back");
+       }else{
+           res.redirect("/apartments/" + req.params.id); //show page
+       }
+   });
+});
+
+module.exports=router;
